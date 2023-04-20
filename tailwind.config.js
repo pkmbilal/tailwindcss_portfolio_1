a7876cc9dae2849f71864f24cba415adc67ee39f
Mohammed <bilal00717@gmail.com>
2023-04-20 13:25:43 +0300

    Fresh Copy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins'
+      }
+    },
+  },
+  plugins: [],
+}
+
